78832ea42867e734b7a8fbc81d0d6d1f1a3a118b
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2024-05-20 13:07:08 +0100

    feat: animate progress value

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -10,24 +10,32 @@ const Progress = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
     indicator?: string
   }
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
+>(({ className, value, ...props }, ref) => {
+  const [progress, setProgress] = React.useState(0)
+  React.useEffect(() => {
+    let timer: string | number | NodeJS.Timeout | undefined
+    if (value) timer = setTimeout(() => setProgress(value), 500)
+    return () => clearTimeout(timer)
+  }, [value])
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
       className={cn(
-        'h-full w-full flex-1 bg-primary transition-all',
-        props.indicator
+        'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
+        className
       )}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-))
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className={cn(
+          'h-full w-full flex-1 bg-primary transition-all',
+          props.indicator
+        )}
+        style={{ transform: `translateX(-${100 - (progress || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  )
+})
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
